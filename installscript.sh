7b7c9f380f1e710f93a75364c0ddc888f4db0e45
Quinten Buis <36452184+quintenbuis@users.noreply.github.com>
2021-04-14 18:34:58 +0200

    Create installscript.sh

---
+++ installscript.sh
@@ -0,0 +1,67 @@
+xcode-select --install
+
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# iterm2
+brew install iterm2
+
+# composer
+brew install composer
+
+# NVM
+brew install nvm # NEEDS EXTRA SETUP
+
+# ZSH
+brew install zsh zsh-autosuggestions # NEEDS EXTRA SETUP
+
+# Valet
+brew install php && composer global require laravel/valet && ~/.composer/vendor/bin/valet install
+
+# Notion
+brew install notion --cask
+
+# alfred
+brew install alfred --cask
+
+# Chrome
+brew install google-chrome --cask
+
+# Git-Flow
+brew install git-flow
+
+# Slack
+brew install slack --cask
+
+# Eul
+brew install eul
+
+# dbngin
+brew install dbngin --cask
+
+# tableplus
+brew install tableplus --cask
+
+# spotify
+brew install spotify --cask
+
+# docker
+brew install docker --cask
+
+# whatsapp
+brew install whatsapp --cask
+
+# phpstorm 
+brew install phpstorm --cask
+
+# spectacle
+brew install spectacle --cask
+
+# tinkerwell
+brew install tinkerwell --cask
+
+# figma
+brew install figma --cask
+
+# postman
+brew install postman --cask
