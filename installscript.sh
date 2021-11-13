7109d6ec22eb59cd8c74145895e30b465f558a50
Quinten <quinten@justbetter.nl>
2021-11-13 19:23:53 +0100

    Update

--- installscript.sh
+++ installscript.sh
@@ -6,15 +6,26 @@ xcode-select --install
 # iterm2
 brew install iterm2
 
+# ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+zsh
+
 # Insert aliases
-curl https://gist.githubusercontent.com/quintenbuis/19606f2697b01acec421a05ffddfc4ed/raw/5a73bfa3574972ad486e1127b1200d4f95d4741c/.aliases > ~/.aliases
+curl https://gist.githubusercontent.com/quintenbuis/dotfiles/main/.aliases > ~/.aliases
+
+curl https://raw.githubusercontent.com/quintenbuis/dotfiles/main/.zshrc > ~/.zshrc
+
+curl https://raw.githubusercontent.com/quintenbuis/dotfiles/main/.vimrc > ~/.vimrc
+curl https://raw.githubusercontent.com/quintenbuis/dotfiles/main/.gitignore > ~/.gitignore
+
+// install spaceship prompt
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 # composer
 brew install composer
 
-# ZSH
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-brew install zsh-autosuggestions # NEEDS EXTRA SETUP
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
@@ -23,70 +34,34 @@ nvm install stable
 # Valet
 brew install php && composer global require laravel/valet && ~/.composer/vendor/bin/valet install
 
-# Notion
-brew install notion --cask
-
-# alfred
-brew install alfred --cask
-
-# Chrome
-brew install google-chrome --cask
-
-# Git-Flow
-brew install git-flow
-
-# Slack
-brew install slack --cask
-
-# Eul
-brew install eul
-
-# dbngin
-brew install dbngin --cask
-
-# tableplus
-brew install tableplus --cask
-
-# spotify
-brew install spotify --cask
-
-# docker
-brew install docker --cask
-
-# whatsapp
-brew install whatsapp --cask
-
-# phpstorm 
-brew install phpstorm --cask
-
-# spectacle
-brew install spectacle --cask
-
-# tinkerwell
-brew install tinkerwell --cask
-
-# spatie/ray
-brew install ray --cask
-
-# figma
-brew install figma --cask
-
-# postman
-brew install postman --cask
-
-# phpmon
 brew tap nicoverbruggen/homebrew-cask
-brew install --cask phpmon
 
-# github cli
-brew install gh
+brew install \
+notion \
+alfred \
+google-chrome \
+slack \
+dbngin \
+tableplus \
+spotify \
+docker \
+whatsapp \
+phpstorm \
+spectacle \
+tinkerwell \
+ray \
+figma \
+postman \
+phpmon \
+cyberduck \
+--cask
 
-# mailhog
-brew install mailhog
-
-# cyberduck
-brew install cyberduck --cask
-
+brew install \
+git-flow \
+eul \
+gh \
+mailhog \
+zsh-autosuggestions \
 
 # add elasticsearch image to docker
 docker run -d --name elasticsearch-793 -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.9.3
