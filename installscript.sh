f3c1f22ef5f48985c1f954109a62f1fd7594ef78
Quinten Buis <quinten.buis@gmail.com>
2025-07-31 14:43:11 +0200

    zsh plugins to install script

--- installscript.sh
+++ installscript.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+# install zsh plugins
 
+## fast-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+## zsh-bat
+git clone https://github.com/fdellwing/zsh-bat.git $ZSH_CUSTOM/plugins/zsh-bat
+
+## zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
